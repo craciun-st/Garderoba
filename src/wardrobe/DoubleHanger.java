0a3c781fec1565799e6d1940feb2758abde61d32
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-03-03 17:07:58 +0200

    Implement DoubleHanger

--- src/wardrobe/DoubleHanger.java
+++ src/wardrobe/DoubleHanger.java
@@ -5,10 +5,91 @@
  */
 package wardrobe;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Adamaltar
  */
-public class DoubleHanger {
-    
+public class DoubleHanger extends Hanger {
+
+    private Clothes top;
+    private Clothes bottom;
+
+    @Override
+    public Clothes takeOffOne(int id) {
+        Clothes temp;
+        if (isClothesOnTopPart(id)) {
+            temp = top;
+            top = null;
+            return temp;
+        }
+        else {
+            if (isClothesOnBottomPart(id)) {
+                temp = bottom;
+                bottom = null;
+                return temp;
+            }
+            else { return null; }
+        }
+    }
+
+    @Override
+    public List<Clothes> takeOffAll() {
+        List<Clothes> clothesOnHanger = new ArrayList<>();
+        if (top != null) {
+            clothesOnHanger.add(takeOffOne(top.getId()));
+        }
+        if (bottom != null) {
+            clothesOnHanger.add(takeOffOne(bottom.getId()));
+        }
+        if (clothesOnHanger.size() > 0) { return clothesOnHanger; }
+        else { return null; }
+    }
+
+    @Override
+    public boolean put(Clothes clothes) {
+        if (top == null || bottom == null) {
+            if (top == null && clothes.getType().isUpperClothes()) {
+                top = clothes;
+                return true;
+            }
+            if (bottom == null && !(clothes.getType().isUpperClothes())) {
+                bottom = clothes;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isRoomFor(ClothesType type) {
+        if ( type.isUpperClothes() ) {
+            return (top == null);
+        }
+        else { return (bottom == null); }
+    }
+
+    @Override
+    protected boolean isClothesOnHanger(int clothesId) {
+        if (top == null && bottom == null) { return false; }
+        else {
+            if (isClothesOnTopPart(clothesId)) { return true; }
+            else {
+                if (isClothesOnBottomPart(clothesId)) { return true; }
+                else { return false; }
+            }
+        }
+    }
+
+    private boolean isClothesOnTopPart(int id) {
+        return (top != null && top.getId() == id);
+    }
+
+    private boolean isClothesOnBottomPart(int id) {
+        return (bottom != null && bottom.getId() == id);
+    }
+
+
 }
